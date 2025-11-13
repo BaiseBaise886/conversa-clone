8fd1b7f98413372d28ae039e6f2ce4d36a745b6e
BaiseBaise886 <rychardbaise@gmail.com>
2025-11-13 07:19:48 -0300

    Remove unused CSS import
    Removed unused CSS import for MultiChatInterface.

--- frontend/src/components/MultiChatInterface.jsx
+++ frontend/src/components/MultiChatInterface.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import './MultiChatInterface.css';
 
 const MultiChatInterface = ({ socket }) => {
   // State declarations
